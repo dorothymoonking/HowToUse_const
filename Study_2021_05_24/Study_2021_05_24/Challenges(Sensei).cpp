c604562ec42541f7771604d107bca46b40890f69
choi <xsc13@naver.com>
2021-05-26 16:27:46 +0900

    End Challenges(Sensei) Project

--- Study_2021_05_24/Study_2021_05_24/Challenges(Sensei).cpp
+++ Study_2021_05_24/Study_2021_05_24/Challenges(Sensei).cpp
@@ -69,19 +69,25 @@ public:
 //----- 정렬 조건 함수
 bool LvSort(const ItemInfo& a, const ItemInfo& b)
 {
-	return a.m_Level > a.m_Level;
+	return a.m_Level > b.m_Level;
 }
 
 bool GradeSort(const ItemInfo& a, const ItemInfo& b)
 {
-	return a.m_Grade < a.m_Grade;
+	return a.m_Grade < b.m_Grade;
 }
 
 bool StarSort(const ItemInfo& a, const ItemInfo& b)
 {
-	return a.m_Star > a.m_Star;
+	return a.m_Star > b.m_Star;
 }
 //-----
+
+//----- 함수 포인터 변수
+int UpKindCount = 3;
+void (*UpGrade_FP[3])(vector<ItemInfo>* a_ItemList);
+//----- 
+
 void LoadItem(vector<ItemInfo>* a_ItemList)
 {
 	FILE* a_rFP = fopen("MySave.abc", "rb");
@@ -127,10 +133,10 @@ void SaveItem(vector<ItemInfo>* a_ItemList)
 
 		fwrite(&g_GameGold, sizeof(int), 1, a_wFP);
 
-		int a_ItemCount = a_ItemList->size();
+		int a_ItemCount = (int)a_ItemList->size();
 		fwrite(&a_ItemCount, sizeof(int), 1, a_wFP);
 
-		for (int ii = 0; ii < a_ItemList->size(); ii++)
+		for (int ii = 0; ii < (int)a_ItemList->size(); ii++)
 		{
 			a_StrCount = strlen((*a_ItemList)[ii].m_Name);
 			fwrite(&a_StrCount, sizeof(int), 1, a_wFP);
@@ -151,11 +157,11 @@ void AddItem(vector<ItemInfo>* a_ItemList)
 
 	int ran = rand() % 8;
 	strcpy(a_TempNode.m_Name, g_ItName[ran]);
-	a_TempNode.m_Level = (rand() % 8) + 1;
-	a_TempNode.m_Grade = 7 - (rand() % 2);
-	a_TempNode.m_Star = (rand() % 8) + 1;
-	a_TempNode.m_Cost = (rand() % 901) + 100;
-	a_ItemList->push_back(a_TempNode);
+a_TempNode.m_Level = (rand() % 8) + 1;
+a_TempNode.m_Grade = 7 - (rand() % 2);
+a_TempNode.m_Star = (rand() % 2) + 1;
+a_TempNode.m_Cost = (rand() % 901) + 100;
+a_ItemList->push_back(a_TempNode);
 }
 
 void PrintList(vector<ItemInfo>* a_ItemList, bool a_isAdd = false)
@@ -164,18 +170,18 @@ void PrintList(vector<ItemInfo>* a_ItemList, bool a_isAdd = false)
 	for (int i = 0; i < a_ItemList->size(); i++)
 	{
 		//-----새로추가된 아이템은 글자색을 변경한다.
-		if (a_isAdd == true && i == (a_ItemList->size() -1))
+		if (a_isAdd == true && i == (a_ItemList->size() - 1))
 		{
 			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE);
 		}
 		//-----
 
-		cout << i + 1 << "번 : "; 
+		cout << i + 1 << "번 : ";
 		(*a_ItemList)[i].PrintInfo();
 
 		if (a_isAdd == true && i == (a_ItemList->size() - 1))
 		{
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); 
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 		}
 	}
 }
@@ -217,14 +223,272 @@ void MyShowSort(vector<ItemInfo>* a_ItemList, int a_SubSel)
 
 void MyItemUpgrade(vector<ItemInfo>* a_ItemList, int a_SubSel)
 {
+	a_SubSel--;
+
+	if (a_SubSel < 0 || UpKindCount <= a_SubSel)
+		return;
+
+	PrintList(a_ItemList);
+	UpGrade_FP[a_SubSel](a_ItemList);
+	SaveItem(a_ItemList);
+}
+
+void LvUpgrade(vector<ItemInfo>* a_ItemList)
+{
+	//cout << "LvUpgrade " << endl;
+	int a_Sel = 0;
+
+	cout << "레벨을 올리고 싶은 아이템 번호를 선택해 주세요(1000골드 감수) : ";
+	cin >> a_Sel;
+	cin.get();
+
+	system("cls");
+	a_Sel--;
+
+	if (a_Sel < 0 || a_ItemList->size() <= a_Sel)
+	{
+		cout << "잘못된 번호를 선택하셨습니다.";
+		return;
+	}
+
+	if (g_GameGold < 1000)
+	{
+		cout << "보유 골드가 부족합니다.";
+		return;
+	}
+	if (30 <= (*a_ItemList)[a_Sel].m_Level)
+	{
+		cout << "이미 최고 레벨입니다.";
+		return;
+	}
+
+	int a_Step = (*a_ItemList)[a_Sel].m_Level;
+	float a_Rate = (31 - a_Step) / 30.0f * 100.0f;
+	cout << "레벨업 확률은 "<< (int)a_Rate << "% 입니다. 계속하시겠습니까? YES(1) NO(2) : ";
+	int a_isGo = 0;
+	cin >> a_isGo;
+	cin.get();
+	if (a_isGo != 1)
+		return;
+
+	for (int ii = 0; ii < 3; ii++) //연출
+	{
+		cout << ". ";
+		Sleep(500);
+	}
+	cout << endl;
+
+	int a_Chance = (rand() % 30) + 2;	//2 ~ 31
+	if (a_Step < a_Chance) //1레벨일 때는 100%, 2레벨 90%, ...29레벨일 때는 0%
+	{
+		(*a_ItemList)[a_Sel].m_Level++;
+		g_GameGold = g_GameGold - 1000;
+		cout << "레벨업에 성공했습니다 +1 레벨 상승" << endl;
+		(*a_ItemList)[a_Sel].m_Cost += 100;
+		(*a_ItemList)[a_Sel].PrintInfo();
+		cout << "남은 보유 머니 : " << g_GameGold << endl;
+	}
+	else
+	{
+		cout << "레벨업 실패ㅠㅠ,";
+		cout << (a_Sel + 1) << "번 " << (*a_ItemList)[a_Sel].m_Name << "(이)가 가루가 되었습니다.";
+		a_ItemList->erase(a_ItemList->begin() + a_Sel);
+		g_GameGold = g_GameGold - 1000;
+		cout << "남은 보유 머니 : " << g_GameGold << endl;
+	}
+}
+
+void GradeUpgrade(vector<ItemInfo>* a_ItemList)
+{
+	//cout << "GradeUpgrade " << endl;
+	int a_Sel_1 = 0;
+	int a_Sel_2 = 0;
+
+	cout << "등급업 할 아이템을 선택해 주세요 (500골드 감소) : ";
+	cin >> a_Sel_1;
+	cin.get();
+
+	cout << "재료 아이템을 선택해 주세요 : ";
+	cin >> a_Sel_2;
+	cin.get();
+
+	system("cls");
+	a_Sel_1--;
+	a_Sel_2--;
+
+	if (a_Sel_1 < 0 || a_ItemList->size() <= a_Sel_1)
+	{
+		cout << "잘못된 번호를 선택하셨습니다." << endl;
+		return;
+	}
 	
+	if (a_Sel_2 < 0 || a_ItemList->size() <= a_Sel_2)
+	{
+		cout << "잘못된 번호를 선택하셨습니다." << endl;
+		return;
+	}
+
+	if (g_GameGold < 500)
+	{
+		cout << "보유 골드가 부족합니다.";
+		return;
+	}
+
+	if (a_Sel_1 == a_Sel_2)
+	{
+		cout << "강화 아이템과 재료 아이템이 일치합니다.";
+		return;
+	}
+
+	//if (strcmp((*a_ItemList)[a_Sel_1].m_Name, (*a_ItemList)[a_Sel_2].m_Name) != 0)
+	//{
+	//	cout << "강화 아이템과 재료 아이템의 종류가 다릅니다.";
+	//	return;
+	//}
+	
+	if ((*a_ItemList)[a_Sel_1].m_Grade <= 1)
+	{
+		cout << "이미 강화 수준이 최대치입니다.";
+		return;
+	}
+
+	int a_Step = (*a_ItemList)[a_Sel_1].m_Grade;
+	float a_Rate = a_Step / 7.0f * 100.0f;
+	cout << "등급업 확률은 " << (int)a_Rate << " % 입니다. 계속 하시겠습니까? YES(1) NO(2)";
+	
+	int a_isGo = 0;
+	cin >> a_isGo;
+	cin.get();
+	if (a_isGo != 1)
+		return;
+
+	for (int ii = 0; ii < 3; ii++) //연출
+	{
+		cout << ". ";
+		Sleep(500);
+	}
+	cout << endl;
+
+	int a_Chance = (int)(rand() % 7); // 1 ~ 6;
+	if (a_Chance < a_Step) //7등급일 때는 100%, 6등급일 때는 85% 
+	{
+		(*a_ItemList)[a_Sel_1].m_Grade--;
+		g_GameGold = g_GameGold - 500;
+		cout << "등급업에 성공했습니다. 1등급 상승" << endl;
+		(*a_ItemList)[a_Sel_1].m_Cost += (7 - (*a_ItemList)[a_Sel_1].m_Grade) * 1000;
+		(*a_ItemList)[a_Sel_1].PrintInfo();
+		cout << "남은 보유 머니 " << g_GameGold;
+		a_ItemList->erase(a_ItemList->begin() + a_Sel_2);
+	}
+
+	else
+	{
+		cout << "강화에 실패했습니다... 아이템을 모두 잃었습니다." << endl;
+		a_ItemList->erase(a_ItemList->begin() + a_Sel_1);
+		if (a_Sel_2 < a_Sel_1)
+		{
+			a_ItemList->erase(a_ItemList->begin() + a_Sel_2);
+		}
+		else if (a_Sel_1 < a_Sel_2)
+		{
+			--a_Sel_2;
+			a_ItemList->erase(a_ItemList->begin() + a_Sel_2);
+		}
+		g_GameGold = g_GameGold - 500;
+		cout << "남은 보유 머니 " << g_GameGold;
+	}
+}
+
+void StarUpgrade(vector<ItemInfo>* a_ItemList)
+{
+	//cout << "StarUpgrade " << endl;
+	int a_Sel_1 = 0;
+	int a_Sel_2 = 0;
+
+	cout << "성급업 확률은 성급업 횟수에 따라 달라집니다." << endl;
+	cout << "성급업 할 아이템을 선택해 주세요 : ";
+	cin >> a_Sel_1;
+	cin.get();
+
+	cout << "재료 아이템을 선택해 주세요 : ";
+	cin >> a_Sel_2;
+	cin.get();
+
+	system("cls");
+	a_Sel_1--;
+	a_Sel_2--;
+	if (a_Sel_1 < 0 || a_ItemList->size() <= a_Sel_1)
+	{
+		cout << "잘못된 번호를 선택하셨습니다.";
+		return;
+	}
+
+	if (a_Sel_2 < 0 || a_ItemList->size() <= a_Sel_2)
+	{
+		cout << "잘못된 번호를 선택하셨습니다.";
+		return;
+	}
+
+	if (a_Sel_1 == a_Sel_2)
+	{
+		cout << "성급업 아이템과 재료 아이템이 일치합니다";
+		return;
+	}
+	
+	if (6 <= (*a_ItemList)[a_Sel_1].m_Star)
+	{
+		cout << "장비가 이미 최고 성급입니다." << endl;
+		return;
+	}
+
+	int a_Step = (*a_ItemList)[a_Sel_1].m_Star;
+	float a_Rate = (7 - a_Step) / 6.0f * 100.0f;
+	cout << "성급업 확률은 " << (int)a_Rate << " % 입니다. 계속 하시겠습니까> YES(1) NO(2) : ";
+	
+	int a_isGo = 0;
+	cin >> a_isGo;
+	cin.get();
+	if (a_isGo != 1)
+		return;
+
+	for (int ii = 0; ii < 3; ii++) //연출
+	{
+		cout << ". ";
+		Sleep(500);
+	}
+	cout << endl;
+
+	int a_Chance = (rand() % 6) + 2; // 2 ~ 7
+	if (a_Step < a_Chance) // 1성급 일 때는 100%, 2성급 일 때는 83%, 3성급 일 때는 66%, ... 5성급 33%
+	{
+		cout << "성급업이 성공했습니다. +1 성급 상승" << endl;
+		(*a_ItemList)[a_Sel_1].m_Star++;
+		(*a_ItemList)[a_Sel_1].m_Cost += (*a_ItemList)[a_Sel_1].m_Star * 1500;
+		(*a_ItemList)[a_Sel_1].PrintInfo();
+		a_ItemList->erase(a_ItemList->begin() + a_Sel_2);
+	}
+	else
+	{
+		cout << "성급업에 실패했습니다. 아이템을 모두 잃었습니다.";
+
+		a_ItemList->erase(a_ItemList->begin() + a_Sel_1);
+		if(a_Sel_2 < a_Sel_1)
+		{
+			a_ItemList->erase(a_ItemList->begin() + a_Sel_2);
+		}
+		else if (a_Sel_1 < a_Sel_2)
+		{
+			a_Sel_2--;
+			a_ItemList->erase(a_ItemList->begin() + a_Sel_2);
+		}
+	}
 }
 
 void SellItem(vector<ItemInfo>* a_ItemList)
 {
 	int a_Sel = 0;
 
-	cout << "판매할 아이템 번호를 선택해 주세요(가격의 80%% 받게 됩니다.) : ";
+	cout << "판매할 아이템 번호를 선택해 주세요(가격의 80% 받게 됩니다.) : ";
 	cin >> a_Sel;
 	cin.get();
 
@@ -275,6 +539,10 @@ void main()
 
 	LoadItem(&ItemList);
 
+	UpGrade_FP[0] = LvUpgrade;
+	UpGrade_FP[1] = GradeUpgrade;
+	UpGrade_FP[2] = StarUpgrade;
+
 	while (true)
 	{
 		cout << "<무기 강화 프로그램> ";
@@ -320,6 +588,7 @@ void main()
 			cout << "레벨(1) 등급(2) 성급(3) : ";
 			cin >> a_SubSel;
 			cin.get();
+			
 			MyItemUpgrade(&ItemList, a_SubSel);
 		}
 
